d2f4b89482d5a696fc2cc569188235852a3bf6ce
JordanBurnett1210 <jabur1210@gmail.com>
2016-02-22 09:51:09 -0700

    updated removeFromFront method.

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -22,3 +22,14 @@ CTECList<Type>::~CTECList()
 	// TODO Auto-generated destructor stub
 }
 
+template <class Type>
+Type CTECList<Type> :: removeFromFront()
+{
+	//Create a pointer to what is after head.
+	ArrayNode<Type> * newHead = new ArrayNode<Type>();
+	newHead = head->getNext();
+	//Delete what head is pointing to.
+	delete this->head;
+	//Set head to the new head.
+	this->head = newHead;
+}
